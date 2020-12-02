48accc766a714402bf6da4cfbc63466556406e32
Nick <nrobertson5595@gmail.com>
2020-12-02 18:36:06 -0500

    last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import people from "./data";
 
 function App() {
+  const checkPoints = (number) => {
+    let points = 0;
+    if (number > 100) {
+      points = (number - 100) * 2 + 50;
+    } else if (number > 50) {
+      points = number - 50;
+    } else {
+      points = number;
+    }
+    return points;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Total Points per month:</h3>
+      <div className="container">
+        <div className="month-oct">
+          {people
+            .filter((person) => {
+              return person.transaction_month === 10;
+            })
+            .map((person) => (
+              <p key={person.id}>
+                {person.name} earned {checkPoints(person.spent)} in October
+              </p>
+            ))}
+        </div>
+        <div className="month-nov">
+          {people
+            .filter((person) => {
+              return person.transaction_month === 11;
+            })
+            .map((person) => (
+              <p key={person.id}>
+                {person.name} earned {checkPoints(person.spent)} in November
+              </p>
+            ))}
+        </div>
+        <div className="month-dec">
+          {people
+            .filter((person) => {
+              return person.transaction_month === 12;
+            })
+            .map((person) => (
+              <p key={person.id}>
+                {person.name} earned {checkPoints(person.spent)} in December
+              </p>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,22 @@
+const transactions = [
+  { id: 1, name: "Nick", spent: 120, transaction_month: 10 },
+  { id: 2, name: "Dave", spent: 100, transaction_month: 10 },
+  { id: 3, name: "Tom", spent: 250, transaction_month: 10 },
+  { id: 4, name: "Rick", spent: 220, transaction_month: 10 },
+  { id: 5, name: "Jake", spent: 75, transaction_month: 10 },
+  { id: 6, name: "Bob", spent: 80, transaction_month: 10 },
+  { id: 7, name: "Nick", spent: 80, transaction_month: 11 },
+  { id: 8, name: "Dave", spent: 100, transaction_month: 11 },
+  { id: 9, name: "Tom", spent: 200, transaction_month: 11 },
+  { id: 10, name: "Rick", spent: 50, transaction_month: 11 },
+  { id: 11, name: "Jake", spent: 0, transaction_month: 11 },
+  { id: 12, name: "Bob", spent: 100, transaction_month: 11 },
+  { id: 13, name: "Nick", spent: 120, transaction_month: 12 },
+  { id: 14, name: "Dave", spent: 50, transaction_month: 12 },
+  { id: 15, name: "Tom", spent: 0, transaction_month: 12 },
+  { id: 16, name: "Rick", spent: 200, transaction_month: 12 },
+  { id: 17, name: "Jake", spent: 60, transaction_month: 12 },
+  { id: 18, name: "Bob", spent: 80, transaction_month: 12 },
+];
+
+export default transactions;
